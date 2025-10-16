3dcd58b3343d01a2f1d49ac628f2f84258c936ce
Edison <premusic314@gmail.com>
2025-10-16 14:57:17 -0500

    clase 2025-10-16

---
+++ modulo2/javascript/02_variables.js
@@ -0,0 +1,37 @@
+console.log("VARIABLES");
+
+saludo = "Hola utilizando variables"
+console.log("Variables Saludo", saludo);
+console.log("Tipo de variable", typeof(saludo));
+
+numero = 256
+console.log("Variable Numero", numero);
+console.log("Tipo de variable", typeof(numero));
+
+booleana = true
+console.log("Variables Booleana", booleana);
+console.log("Tipo de variable", typeof(booleana));
+
+indefinida = undefined
+console.log("Variables Indefinida", indefinida);
+console.log("Tipo de variable", typeof(indefinida));
+
+nula = null
+console.log("Variables Nula",nula);
+console.log("Tipo de variable", typeof(nula));
+
+bigentero = 876567529837912837197858n
+console.log("Variables BigInt", bigentero);
+console.log("Tipo de variable", typeof(bigentero));
+
+persona = {
+    nombre: "carlos",
+    edad: 30
+}
+
+console.log("Variable Objeto", persona);
+console.log("Tipo de variable ", typeof(persona));
+
+numeros = [12,12,22,34,4,4]
+console.log("Variable Array ", numeros)
+console.log("Tipo de Variable ", typeof(numeros));
