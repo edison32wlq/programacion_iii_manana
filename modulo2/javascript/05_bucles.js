c15a44fa329148a7b1170f941712b8d5c0a4d02c
Edison <premusic314@gmail.com>
2025-10-21 10:00:55 -0500

    clase 2025-20-21

---
+++ modulo2/javascript/05_bucles.js
@@ -0,0 +1,21 @@
+for (let i=1; i <=10; i++){
+    console.log("numero", i);
+}
+
+suma = 0;
+for (let i=1; i <=10; i++){
+    suma += i;
+}
+
+console.log("sumatoria indices", suma);
+
+for (let i=1; i <=10; i++){
+    m = 4 * i;
+    console.log("4 * ", i , " = ", m);
+}
+
+
+nombre = "edison";
+for (let i=0; i <nombre.length; i++){
+    console.log(nombre[i]);
+}
