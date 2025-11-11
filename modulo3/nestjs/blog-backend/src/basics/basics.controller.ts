8cde54aa4e2fde36babbe3cfc8cdc9e529b41670
Edison <premusic314@gmail.com>
2025-11-11 10:06:39 -0500

    clase 2025-11-11

---
+++ modulo3/nestjs/blog-backend/src/basics/basics.controller.ts
@@ -0,0 +1,33 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { BasicsService } from './basics.service';
+
+@Controller('basics')
+export class BasicsController {
+    constructor(private readonly basicsService : BasicsService) {}
+
+        @Get()
+        getMyFisrtGet(): object {
+            return this.basicsService.getMyFisrtGet();
+        }
+
+        @Get(':parametro')
+        getConParametros(@Param('parametro') parametro: string) {
+            return this.basicsService.getConParametros(parametro);
+        }
+
+        @Post()
+        create(@Body() bodyPost: object) {
+            return this.basicsService.postFunction(bodyPost);        
+        }
+
+        @Put(':id')
+        update(@Body() bodyPost: object, @Param('id') id: string) {
+            return this.basicsService.putFunction(bodyPost,id);
+        }
+        
+        @Delete(':id')
+        delete(@Param('id') id: string) {
+            return this.basicsService.deleteFunction(id);
+        }
+
+}
---
+++ modulo3/nestjs/blog-backend/src/basics/basics.service.ts
@@ -0,0 +1,51 @@
+import { createParamDecorator, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class BasicsService {
+
+    getMyFisrtGet(): object {
+        return{
+            service:'Blog-Backend',
+            controller: '/basics',
+            function: 'Get Example'
+        };
+    }
+
+    getConParametros(parametro: string): object {
+        return{
+            service:'Blog-Backend',
+            controller: '/basics',
+            function: 'Get con Parametros',
+            parametroRecibido: parametro
+        };
+    }
+
+    postFunction(bodyPost: object): object {
+        return {
+            service: 'Blog-Backend',
+            controller: '/basics tipo post',
+            function: 'Ejemplo de peticion Post',
+            bodyRecibido: bodyPost
+        };
+    }
+
+    putFunction(bodyPost: object, parametro: string): object {
+        return {
+            service: 'Blog-Backend',
+            controller: '/basics tipo put',
+            function: 'Ejemplo de peticion Put',
+            bodyRecibido: bodyPost,
+            parametro: parametro
+        };
+    }
+
+    deleteFunction(parametro: string): object {
+        return {
+            service: 'Blog-Backend',
+            controller: '/basics tipo delete',
+            function: 'Ejemplo de peticion Delete',
+            parametro: parametro
+        };
+    }
+
+}
